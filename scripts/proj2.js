87be61178c341f1656ea9c8b55b412f4e4e4b536
Elle Jo <joe074@newschool.edu>
2020-02-28 15:48:49 -0500

    change

---
+++ scripts/proj2.js
@@ -0,0 +1,21 @@
+$("#color1").click(function(){
+	$(".color1").toggleClass("showup");
+	// $(".color1").css("opacity",1);
+	
+});
+
+
+$("#color2").click(function(){
+	$("#color2").toggleClass("showup");
+	// $(".t2").css("opacity",1);
+});
+
+$("#btn3").click(function(){
+	$(".t3").toggleClass("showup");
+	//$(".t3").css("opacity",1);
+});
+
+$("#btn4").click(function(){
+	$(".t4").toggleClass("showup");
+	//$(".t4").css("opacity",1);
+});
